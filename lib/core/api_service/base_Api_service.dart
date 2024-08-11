fa31e738516ee7bdb5411ff335130652b740433a
Rae1850 <riorae436@gmail.com>
2024-08-11 23:09:25 +0300

    the search and the suggest

--- lib/core/api_service/base_Api_service.dart
+++ lib/core/api_service/base_Api_service.dart
@@ -17,7 +17,7 @@ abstract class BaseApiService {
     required String? attributeName,
     required List<File>? files,
   });
-  Future<dynamic> deleteRequest({required String url});
+  Future<dynamic> delete({required String url});
   Future<dynamic> getRequest2(
       {required String url, Map<String, String>? parameters});
 }
